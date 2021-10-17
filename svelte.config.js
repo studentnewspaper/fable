a5171e275b27204c15e187985a61e1e15eea4524
Nicholas Bush <nickdbush@hotmail.co.uk>
2021-10-17 23:51:15 +0100

    Remove commit SHA and Sentry

--- svelte.config.js
+++ svelte.config.js
@@ -2,19 +2,6 @@ import preprocess from 'svelte-preprocess';
 import path from 'path';
 import node from '@sveltejs/adapter-node';
 
-const sha = process.env.VITE_GIT_COMMIT_SHA ?? null;
-if (sha != null) {
-	if (typeof sha == 'string') {
-		console.log(`Running fable:${sha}`);
-		console.log(`SHA has a length of ${sha.length}`);
-	} else {
-		console.log(`SHA of unexpected type: ${typeof sha}`);
-		console.log(sha);
-	}
-} else {
-	console.log(`No release SHA available`);
-}
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
